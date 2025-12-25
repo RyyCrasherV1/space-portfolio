93785601e462b1fa35b3d0e026b4ddded861bd0c
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2025-12-26 02:53:37 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,9 @@
+
 import type { Metadata } from "next";
 
 export const siteConfig: Metadata = {
-  title: "Reyy Anti Meninggi | Space Portfolio",
-  description: "Welcome to   description: "Welcome to my Porto Reyy full stack N,
+  title: "John Doe | Space Portfolio",
+  description: "Welcome to my full stack Next.js 14 space portfolio.",
   keywords: [
     "reactjs",
     "nextjs",
@@ -35,7 +36,7 @@ export const siteConfig: Metadata = {
     "css",
   ] as Array<string>,
   authors: {
-    name: "Reyy Shinn.",
-    url: "belum ada",
+    name: "Reyy Nihh Dek",
+    url: "",
   },
 } as const;
